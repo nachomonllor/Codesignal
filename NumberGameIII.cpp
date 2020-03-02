fc20996c8179fc3f5b666e20a7b0282ea7c88b9d
ignacio <nachomonllorc@gmail.com>
2020-03-02 04:39:55 -0300

    agrego en .doc

---
+++ NumberGameIII.cpp
@@ -0,0 +1,32 @@
+﻿https://codefights.com/challenge/kA3KCeg6aPYpDLaiQ
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+ 
+using namespace std;
+ 
+int NumberGameIII(std::vector<int> S) {
+   
+    for(int i = 0; i < S.size(); i++)
+    {  
+        bool divisorDeTodos = true;
+        for(int j = 0; j < S.size(); j++){
+            if(S[j] % S[i] != 0){
+                divisorDeTodos = false;
+                break;
+            }
+        }
+       
+        if(divisorDeTodos){
+            return S[i];
+        }
+       
+    }
+    return -1;
+   
+}
+ 
+int main(){
+    //NumberGameIII([3,6,3,15,9])
+    return 0;
+}
---
+++ zeros.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+int zeros(int n) {
+	int answer = 0;
+	while(n > 0) {
+		 
+		if(n % 2 == 0) {
+			answer ++;
+		}
+		n/=2;
+	}
+	return answer;
+}
+
+int main() {
+	
+	int n = zeros(5);
+	printf("%d ", n);
+	return 0;
+}
